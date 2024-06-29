84a71236f7dc43e753280a547d0d85dec653b7a0
yuki26 <yukisakurai929@gmail.com>
2024-06-30 02:31:01 +0900

    first commit

---
+++ qrmaker.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from PIL import Image
+import qrcode
+
+
+#ボタンを右寄せ
+st.markdown("""
+    <style>
+    .stButton button {
+        float: right;
+    }
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }    
+    </style>
+    """, unsafe_allow_html=True)
+
+
+#タイトル
+st.title('QRメーカー')
+
+
+#フォームの作製
+with st.form(key='profile_form'):   #ボタンが押されたときに画面をリロード
+    #テキストボックス
+    url = st.text_input('生成したいURLを入力してください:')
+    submit_btn = st.form_submit_button('QRコードを生成')
+    print(url)
+
+
+#QRコードの生成
+if submit_btn:
+    img = qrcode.make(url)
+    img = img.convert('RGB')
+    st.image(img)
+    st.write(url)
